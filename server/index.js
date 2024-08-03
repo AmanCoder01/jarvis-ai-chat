c6260cedbd44390779f6945f9112f939ddd6bf5d
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2024-08-03 11:26:56 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -167,13 +167,13 @@ app.use((err, req, res, next) => {
 
 // // PRODUCTION
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "/client/dist")))
+// app.use(express.static(path.join(__dirname, "/client/dist")))
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-})
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// })
 
 
 app.listen(port, () => {
